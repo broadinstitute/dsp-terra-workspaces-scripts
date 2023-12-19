94a16d89adc1e3d1b2137697272aca7c96e72836
aherbst-broad <aherbst@broadinstitute.org>
2023-12-19 10:55:20 -0500

    Add get method

--- tools/workspace.py
+++ tools/workspace.py
@@ -6,6 +6,9 @@ import argparse
 import requests
 import logging
 import sys
+import json
+
+from requests import HTTPError
 
 from utils import auth, poll, cli
 from utils.conf import Configuration
@@ -16,7 +19,23 @@ logging.basicConfig(
 )
 
 
-def get_workspace(
+def get_workspace_by_id(id: str, session: requests.Session):
+    """
+    Gets the workspace from rawls
+    :param id:
+    :return:
+    """
+    rawls_host = Configuration.get_config()["rawls_host"]
+    url = f"{rawls_host}/api/workspaces/id/{id}"
+    token = auth.get_gcp_token()
+
+    headers = auth.build_auth_headers(token)
+    workspace_response = session.get(url=url, headers=headers)
+
+    return workspace_response
+
+
+def get_workspace_by_name(
     workspace_name: str, billing_project_name: str, session: requests.Session
 ):
     """
@@ -30,8 +49,9 @@ def get_workspace(
     token = auth.get_gcp_token()
 
     headers = auth.build_auth_headers(token)
-    raw_status = session.get(url=url, headers=headers)
-    return raw_status
+    workspace_response = session.get(url=url, headers=headers)
+
+    return workspace_response
 
 
 def delete_workspace(workspace_name: str, billing_project_name: str):
@@ -45,7 +65,9 @@ def delete_workspace(workspace_name: str, billing_project_name: str):
     headers = auth.build_auth_headers(token)
 
     session = get_session_with_retry()
-    workspace_response = get_workspace(workspace_name, billing_project_name, session)
+    workspace_response = get_workspace_by_name(
+        workspace_name, billing_project_name, session
+    )
     if workspace_response.status_code == 404:
         logging.info(
             f"Workspace {billing_project_name}/{workspace_name} is gone, skipping deletion."
@@ -70,7 +92,9 @@ def delete_workspace(workspace_name: str, billing_project_name: str):
         )
 
     def deletion_poller():
-        raw_status = get_workspace(workspace_name, billing_project_name, session)
+        raw_status = get_workspace_by_name(
+            workspace_name, billing_project_name, session
+        )
         if raw_status.status_code == 404:
             return True, None
 
@@ -96,12 +120,54 @@ def _delete_workspace_cmd(args):
     delete_workspace(args.workspace_name, args.billing_project_name)
 
 
+def _get_workspace_cmd(args):
+    if (
+        args.workspace_name is None
+        and args.billing_project_name is None
+        and args.workspace_id is None
+    ):
+        raise Exception("Must specify either workspace name or workspace id")
+
+    if args.workspace_id:
+        try:
+            workspace_response = get_workspace_by_id(
+                args.workspace_id, get_session_with_retry()
+            )
+            workspace_response.raise_for_status()
+            logging.info(json.dumps(workspace_response.json(), indent=4))
+        except HTTPError as e:
+            if e.response.status_code == 404:
+                logging.warning(f"Workspace with id {args.workspace_id} not found")
+            else:
+                raise e
+    else:
+        workspace_response = get_workspace_by_name(
+            args.workspace_name,
+            args.billing_project_name,
+            get_session_with_retry(),
+        )
+        try:
+            workspace_response.raise_for_status()
+            logging.info(json.dumps(workspace_response.json(), indent=4))
+        except HTTPError as e:
+            if e.response.status_code == 404:
+                logging.warning(
+                    f"Workspace with name {args.billing_project_name}/{args.workspace_name} not found"
+                )
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("-u", "--user_token", required=False)
 
     subparsers = parser.add_subparsers()
     subparsers.required = True
+
+    get_subparser = subparsers.add_parser("get")
+    get_subparser.add_argument("-w", "--workspace_name", required=False)
+    get_subparser.add_argument("-bp", "--billing_project_name", required=False)
+    get_subparser.add_argument("-i", "--workspace_id", required=False)
+    get_subparser.set_defaults(func=_get_workspace_cmd)
 
     delete_subparser = subparsers.add_parser("delete")
     delete_subparser.add_argument("-w", "--workspace_name", required=True)
